63a48f08c176c63f78aa009afaeb3b055d8645c5
Robert Hutchins <rjhutchins22@gmail.com>
2017-08-27 11:18:58 -0400

    fixed link

--- src/components/work.js
+++ src/components/work.js
@@ -15,7 +15,7 @@ export default class Work extends Component {
               <p className="project-text">NuÜ is a ficticous marketplace for social media enhancement services. It is built as a single page app with React for the front end and, Node as the backend. I personally handled the administration page, API, and backend build for this project. Checkout the <a href="https://github.com/RagyHutch/TIS-store" className="para-link">backend</a> code for the project.</p>
               <span className="tools-used">React, Node, Express, Mongo, HTML5, CSS3, Express, Bootstrap</span>
               <br />
-              <a className="project-button live-btn" href="http://nuu.surge.sh" target="_blank"><button className="project-button">Live Site</button></a>
+              <a className="project-button live-btn" href="http://nuu.surge.sh/" target="_blank"><button className="project-button">Live Site</button></a>
               <a className="project-button github-btn" href="https://github.com/mykltronn/The-Iron-Store" target="_blank"><button className="project-button">GitHub</button></a>
             </section>
           </div>
